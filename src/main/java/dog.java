a9759b823755c733067983921677a1318be05393
hyt05712023 <hyt05712023@163.com>
2023-05-22 10:07:17 +0800

    查看系统第一次提交

--- src/main/java/dog.java
+++ src/main/java/dog.java
@@ -2,5 +2,6 @@ public class dog {
     public static void main(String[] args) {
         System.out.println("Hello Github");
         System.out.println("add source");
+        System.out.println("I am a dog");
     }
 }
